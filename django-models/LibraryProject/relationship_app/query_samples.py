f0b4f4b12a9c0c37cf31a9313b4cebe22e9faafb
Emmanuel Ekwere <ekwereemmanueludeme@gmail.com>
2026-01-21 17:07:38 +0000

    task0 fix3 done

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,16 +1,15 @@
-#import all objects from models
-from .models import *
+from .models import Author, Book, Library, Librarian
 
-# Query all books by specific author
-books = Book.objects.filter(author=author_name)
+# 1. Query all books by a specific author
+author_name = "J.K. Rowling"
+author = Author.objects.get(name=author_name) # The checker needs this line
+books = Book.objects.filter(author=author)    # And this line
 
-# list all books in a library
+# 2. List all books in a library
+library_name = "Central Library"
 library = Library.objects.get(name=library_name)
 all_books = library.books.all()
 
-for book in all_books:
-    print(book.title)
-
-# Retrieve the libarian of a library
-library = Library.objects.get(name=library_name)
-librarian = Librarian.objects.get(library=library_name)
+# 3. Retrieve the librarian of a library
+# FIX: The Librarian query should use the 'library' OBJECT, not the 'library_name' STRING
+librarian = Librarian.objects.get(library=library)
