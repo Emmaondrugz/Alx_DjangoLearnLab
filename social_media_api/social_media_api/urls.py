d0560ef815bbe827af723758188d30e642b09739
Emmanuel Ekwere <ekwereemmanueludeme@gmail.com>
2026-02-19 12:46:22 +0000

    Added api/ to general urls.py path

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
-   # path('posts/urls', include('posts.urls')),
+    path('api/accounts/', include('accounts.urls')),
+    path('api/posts/urls', include('posts.urls')),
 ]
