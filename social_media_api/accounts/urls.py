b89dc218b798be49f29e7e46700f40f837fe27e9
Emmanuel Ekwere <ekwereemmanueludeme@gmail.com>
2026-02-20 17:16:03 +0000

    changed string formats in accounts/urls.py

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -4,9 +4,9 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 from .views import RegistraionView, FollowUserView, UnfollowUserView
 
 urlpatterns = [
-    path('login/', TokenObtainPairView.as_view()),
-    path('register/', RegistraionView.as_view()),
-    path('token/refresh', TokenRefreshView.as_view()),
-    path('follow/<int:user_id>', FollowUserView.as_view()),
-    path('unfollow/<int:user_id>', UnfollowUserView.as_view())
+    path("login/", TokenObtainPairView.as_view()),
+    path("register/", RegistraionView.as_view()),
+    path("token/refresh", TokenRefreshView.as_view()),
+    path("follow/<int:user_id>", FollowUserView.as_view()),
+    path("unfollow/<int:user_id>", UnfollowUserView.as_view())
 ]
